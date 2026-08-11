a7b0010c2a6d761ac78864cce340ea6d86be599c
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-08-12 00:17:09 +0530

    Find the starting point in LL

--- 06 Linked List Part II/05_Find_The_Starting_Point_In_LL.py
+++ 06 Linked List Part II/05_Find_The_Starting_Point_In_LL.py
@@ -34,3 +34,30 @@ class Solution:
             st.add(head)
             head = head.next
         return None
+
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                slow = head
+
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
